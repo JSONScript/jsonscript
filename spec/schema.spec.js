8b9642f47bf98ffe70058049e4440799adf71b8f
Evgeny Poberezkin <e.poberezkin@me.com>
2016-02-11 22:36:16 +0000

    evaluate_metaschema.json

--- spec/schema.spec.js
+++ spec/schema.spec.js
@@ -3,8 +3,10 @@
 var jsonSchemaTest = require('json-schema-test')
   , Ajv = require('ajv');
 
-var ajv = Ajv({ allErrors: true });
-ajv.addSchema(require('../schema'));
+var ajv = Ajv({ allErrors: true, v5: true });
+ajv.addSchema(require('../schema/schema'));
+ajv.addMetaSchema(require('../schema/evaluate_metaschema'));
+ajv.addSchema(require('../schema/evaluate'));
 
 
 jsonSchemaTest([ ajv ], {

ef9792d03506c7b02c80bdc2d99c3017fdcdea61
Evgeny Poberezkin <e.poberezkin@me.com>
2016-02-14 20:53:17 +0000

    evaluate schema changes

--- scripts/generate.js
+++ scripts/generate.js
@@ -4,27 +4,26 @@ var fs = require('fs');
 var doT = require('dot');
 var instructions = require('../instructions');
 
-
 generateSchema('schema');
 generateSchema('evaluate');
 generateSchema('evaluate_metaschema');
 
 
 function generateSchema(schemaName) {
-    var template = getSchemaTemplate(schemaName);
-    var schemaStr = template({ instructions: instructions });
-    schemaStr = JSON.stringify(JSON.parse(schemaStr), null, '  ');
-    fs.writeFileSync(getFileName(schemaName), schemaStr);
+  var template = getSchemaTemplate(schemaName);
+  var schemaStr = template({ instructions: instructions });
+  schemaStr = JSON.stringify(JSON.parse(schemaStr), null, '  ');
+  fs.writeFileSync(getFileName(schemaName), schemaStr);
 }
 
 
 function getSchemaTemplate(schemaName) {
-    var fileName = getFileName(schemaName) + '.dot';
-    var tempalteStr = fs.readFileSync(fileName, 'utf8');
-    return doT.compile(tempalteStr);
+  var fileName = getFileName(schemaName) + '.dot';
+  var templateStr = fs.readFileSync(fileName, 'utf8');
+  return doT.compile(templateStr);
 }
 
 
 function getFileName(schemaName) {
-    return __dirname + '/../schema/' + schemaName + '.json';
+  return __dirname + '/../schema/' + schemaName + '.json';
 }
